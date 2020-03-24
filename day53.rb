7315f3575c91baaf7511d7e4af6c04f1564631bd
shanghuifu <shanghuifu@gmail.com>
2020-03-24 17:54:22 +0900

    仕様が決められたrailsアプリケーションの作成

---
+++ day53.rb
@@ -0,0 +1,17 @@
+# 以下の仕様を満たすRailsアプリケーションを作成してください。ただし、scaffoldを使用して構いません。
+# ・authorsテーブルがある
+# ・booksテーブルがある
+# ・authorsとbooksは１対多のアソシエーションが組まれている
+# ・authorsテーブルのレコードを削除すると、関連するbooksテーブルのレコードも同時に削除される
+# また、railsコンソールを使用して正しい挙動になっていることを確認してください。
+
+
+# terminal
+# > rails new sample-app -d mysql
+# > rails g scaffold author name:string
+# > rails g scaffold book name:string author:references
+
+# author.rb
+# class Author < ApplicationRecord
+#   has_many :books ,dependent: :destroy
+# end
